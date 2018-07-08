7e9e4bf10304b08f40247c32642b5eb3de3912dc
许庭淞 <318715498@qq.com>
2018-07-08 16:53:43 +0800

    change pageoffset to 3 page

--- Sex8BlockExtension/CloudFetchBot/ViewController.swift
+++ Sex8BlockExtension/CloudFetchBot/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: NSViewController {
     var tor: IndexingIterator<[ListCategrory]>?
     var site = Site.netdisk
     var startIndex: UInt = 1
-    let pageOffset: UInt = 5
+    let pageOffset: UInt = 3
     var startTime: Date!
     @IBOutlet weak var action: NSButton!
     @IBOutlet weak var label: NSTextField!
@@ -68,6 +68,7 @@ class ViewController: NSViewController {
             bot.pageOffset = self.pageOffset
             self.site.categrory = self.tor?.next()
             DispatchQueue.global().async {
+                 [unowned self] in
                 bot.start(withSite: self.site)
             }
         })
@@ -82,13 +83,14 @@ extension ViewController : FetchBotDelegate, CloudSaver {
         let message = "+++++++ 正在接收 \(index) 项数据..."
         print(message)
         DispatchQueue.main.async {
+             [unowned self] in
             self.label.stringValue = message
         }
         guard let s = site.categrory?.site  else {
             return
         }
         
-        check(title: content.page) { (res) in
+        check(title: content.page) { [unowned self] (res) in
             print(res)
             if res.count <= 0 {
                 self.save(netDisk: CloudData(contentInfo: content, site: s)) { (rec, err) in
@@ -117,18 +119,20 @@ extension ViewController : FetchBotDelegate, CloudSaver {
     func bot(_ bot: FetchBot, didFinishedContents contents: [ContentInfo], failedLink : [FetchURL]) {
         let message = ">>>>>>>>>>>> 已成功接收 \(bot.count - failedLink.count) 项数据, \(failedLink.count) 项接收失败, spend time: \(Date().timeIntervalSince(startTime))"
         print(message)
-        DispatchQueue.main.async {
+        DispatchQueue.main.async { [unowned self] in
             self.label.stringValue = message
         }
         let bot = FetchBot.shareBot
         site.categrory = tor?.next()
         guard let _ = site.categrory else {
             DispatchQueue.main.async {
+                 [unowned self] in
                 self.action.isEnabled = true
             }
             return
         }
         DispatchQueue.global().async {
+             [unowned self] in
             bot.start(withSite: self.site)
         }
     }
