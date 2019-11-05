807862eab7b8e705ac98a2f3db7bc50e54c01fe0
John Conner <virus1993@icloud.com>
2019-11-06 00:03:08 +0800

    test file.

---
+++ Sex8BlockExtension/Sex8BlockExtension/SwiftUI/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  Sex8BlockExtension
+//
+//  Created by god on 2019/10/7.
+//  Copyright © 2019 ascp. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
